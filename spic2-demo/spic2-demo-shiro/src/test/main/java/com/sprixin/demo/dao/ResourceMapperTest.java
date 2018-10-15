c5a1e920f0a144f8e0fcbf0f89a19469c78aa6b7
SUMN <supermoon0201@163.com>
2018-10-15 20:44:14 +0800

    添加单元测试

---
+++ spic2-demo/spic2-demo-shiro/src/test/main/java/com/sprixin/demo/dao/ResourceMapperTest.java
@@ -0,0 +1,41 @@
+/**
+ * Project Name:spic2-demo-shiro
+ * File Name:ResourceMapperTest.java
+ * Package Name:com.sprixin.demo.dao
+ * Date:2018年10月15日下午6:51:51
+ * Copyright (c) 2018, SPRIXIN.com All Rights Reserved.
+ */
+package com.sprixin.demo.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.sprixin.demo.bootstrap.ShiroApplication;
+
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 单元测试<br/>
+ * <br/>
+ * 创建时间： 2018年10月15日 下午6:51:51 <br/>
+ * 
+ * @author le.yang
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShiroApplication.class)
+public class ResourceMapperTest {
+    @Autowired
+    private ResourceMapper resourceMapper;
+
+    @Test
+    public void testListResourceUrlByRoleId() {
+        Set<String> urlSet = resourceMapper.listResourceUrlByRoleId((long) 5);
+        assertNotNull(urlSet);
+    }
+
+}
---
+++ spic2-demo/spic2-demo-shiro/src/test/main/java/com/sprixin/demo/dao/RoleMapperTest.java
@@ -0,0 +1,38 @@
+/**
+ * Project Name:spic2-demo-shiro
+ * File Name:RoleMapperTest.java
+ * Package Name:com.sprixin.demo.dao
+ * Date:2018年10月15日下午7:14:49
+ * Copyright (c) 2018, SPRIXIN.com All Rights Reserved.
+ */
+package com.sprixin.demo.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.sprixin.demo.bootstrap.ShiroApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 角色DAO单元测试<br/>
+ * <br/>
+ * 创建时间： 2018年10月15日 下午7:14:49 <br/>
+ * 
+ * @author le.yang
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ShiroApplication.class)
+public class RoleMapperTest {
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Test
+    public void testListRoleByUserId() {
+        assertNotNull(roleMapper.listRoleByUserId((long) 6));
+    }
+
+}
